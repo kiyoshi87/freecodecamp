563af84a4f9ae0ca7dfaa66991ce01f7e7429bdb
Steven K <kiyoshi87@users.noreply.github.com>
2024-02-27 16:21:53 +0800

    update sea_level_predictor.py

--- data-analysis-with-python/data-analysis-with-python-projects/sea-level-predictor/sea_level_predictor.py
+++ data-analysis-with-python/data-analysis-with-python-projects/sea-level-predictor/sea_level_predictor.py
@@ -1,36 +1,53 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
-import numpy as np
 
 def draw_plot():
     # Read data from file
-    df = pd.read_csv('epa-sea-level.csv')
+    df = pd.read_csv("epa-sea-level.csv")
+  
+    # Create FIRST line of best fit
+    x = df['Year']
+    y = df['CSIRO Adjusted Sea Level']
 
-    # Create scatter plot
-    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
+    # First grab the current Slope & Intercept values
+    slope, intercept, r_value, p_value, std_err = linregress(x, y)
 
-    # Create first line of best fit
-    lineA = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
-    xA = np.arange(df['Year'].min(),2050,1)
-    yA = xA*lineA.slope + lineA.intercept
+    # Using the above Slope/ Intercept values now plot Best Fit 1
+    x1 = list(range(1880, 2050)) 
+    y1 = []
 
-    plt.plot(xA,yA)
+    for year in x1:
+      y1.append(intercept + slope * year)
 
-    # Create second line of best fit
-    df_2000 = df[df['Year'] >= 2000]
+    plt.plot(x1, y1, 'r', label = 'Best Fit Line 1')
+    plt.legend()
+    # plt.show()
+    
+    # Create Second line of Best fit
+    # SECOND best fit for year 2000 onwards so shall have new slope and intercept
 
-    lineB = linregress(df_2000['Year'], df_2000['CSIRO Adjusted Sea Level'])
-    xB = np.arange(2000,2050,1)
-    yB = xB*lineB.slope + lineB.intercept
+    xfuture = df[df['Year'] >= 2000] ['Year']
+    yfuture = df[df['Year'] >= 2000] ['CSIRO Adjusted Sea Level']
 
-    plt.plot(xB,yB)
+    newfit = linregress(xfuture, yfuture)
+    newslope = newfit.slope
+    newintercept = newfit.intercept
+
+    xfuture2 = list(range(2000, 2050))
+    yfuture2 = []
+
+    for xfuture in xfuture2:
+      yfuture2.append(newintercept + newslope * xfuture)
+        
+    plt.plot(xfuture2, yfuture2, 'r', label = 'Best Fit Line 2', color='green')
+    plt.legend()
+    plt.show()
 
     # Add labels and title
     plt.xlabel('Year')
     plt.ylabel('Sea Level (inches)')
     plt.title('Rise in Sea Level')
-    
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
